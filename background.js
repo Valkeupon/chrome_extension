4a2f45a8c380d510c1e31fb5f393f420db6c44f2
valentin messiaen <valentin.messiaen@gmail.com>
2018-11-02 16:04:30 +0100

    add firebase + insert

--- background.js
+++ background.js
@@ -1,14 +1,29 @@
+const config = {
+    apiKey: "AIzaSyAp5Xbc_MJhPbCC9867fDEBiejU8o_wYhU",
+    authDomain: "extensionchrome-a6490.firebaseapp.com",
+    databaseURL: "https://extensionchrome-a6490.firebaseio.com",
+    projectId: "extensionchrome-a6490",
+    storageBucket: "",
+    messagingSenderId: "988181996502"
+};
+firebase.initializeApp(config);
+const db = firebase.firestore();
+db.settings({
+  timestampsInSnapshots: true
+});
+
 chrome.runtime.onInstalled.addListener(function() {
-   // chrome.storage.sync.set({color: '#3aa757'}, function() {
+   // chrome.storage.sync.set(messages, function() {
    //   console.log("The color is green.");
    // });
+
    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
           pageUrl: { hostContains: '.' },
         })
         ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
+        actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
 
@@ -20,9 +35,38 @@ chrome.runtime.onInstalled.addListener(function() {
       }
       //CREATED
       if(message.type == "created"){
-        alert(message.content);
-        //executeScript
+        if(!message.content){
+          sendResponse({ type: "error", content: "Aucun message" });
+          return;
+        }
+        db.collection("messages").add({
+            content: message.content
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
       }
+
+      if(message.type == "get"){
+        const messages = [];
+        db.collection("messages").get().then(function(elem){
+          if(!elem){
+            messages = null;
+          }
+          elem.forEach((doc) => {
+            const message = doc.data();
+            messages.push(message.content);
+          });
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+        console.log(messages);
+        return sendResponse(messages);
+      }
+
     }
     chrome.runtime.onMessage.addListener(onMessageListener);
- });
+});
--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 let changeValue = document.getElementById('addValue');
 let input = document.getElementById('inputValue');
-
+chrome.runtime.sendMessage({ type: "get" }, (res) => {
+  console(res);
+});
 // chrome.storage.sync.get('color', function(data) {
 //   changeColor.style.backgroundColor = data.color;
 //   changeColor.setAttribute('value', data.color);
@@ -14,7 +16,11 @@ const console = function(params) {
 
 changeValue.onclick = function(element) {
     let value = input.value;
-    chrome.runtime.sendMessage({ type: "created", content: value });
+    chrome.runtime.sendMessage({ type: "created", content: value }, (err) => {
+      if(err.type == "error"){
+        alert(err.content);
+      }
+    });
     // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     //   chrome.tabs.executeScript(
     //       tabs[0].id,
